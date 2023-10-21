970c4c2e5fc59b6c2374cb017f0145b042b7f29a
moneydontbuyclass <38925895+moneydontbuyclass@users.noreply.github.com>
2023-10-21 12:04:07 -0400

    updated add.sh

--- Docker/Nextcloud/NCImage/nextcloud/add.sh
+++ Docker/Nextcloud/NCImage/nextcloud/add.sh
@@ -2,20 +2,20 @@
 
 sed -i "/);/i 'default_phone_region' => 'US'," /var/www/html/config/config.php
 sed -i "/);/i 'overwriteprotocol' => 'https'," /var/www/html/config/config.php
-sed -i "/);/r /dev/stdin" /var/www/html/config/config.php <<EOF
-'enable_previews' => true,
-'enabledPreviewProviders' =>
-  array (
-    0 => 'OC\\Preview\\Movie',
-    1 => 'OC\\Preview\\PNG',
-    2 => 'OC\\Preview\\JPEG',
-    3 => 'OC\\Preview\\GIF',
-    4 => 'OC\\Preview\\BMP',
-    5 => 'OC\\Preview\\XBitmap',
-    6 => 'OC\\Preview\\MP3',
-    7 => 'OC\\Preview\\MP4',
-    8 => 'OC\\Preview\\TXT',
-    9 => 'OC\\Preview\\MarkDown',
-    10 => 'OC\\Preview\\PDF',
-  ),
-EOF
+
+sed -i "/);/i 'enable_previews' => true," /var/www/html/config/config.php
+sed -i "/);/i 'enabledPreviewProviders' =>" /var/www/html/config/config.php
+sed -i "/);/i array (" /var/www/html/config/config.php
+sed -i "/);/i 0 => 'OC\\Preview\\Movie'," /var/www/html/config/config.php
+sed -i "/);/i 1 => 'OC\\Preview\\PNG'," /var/www/html/config/config.php
+sed -i "/);/i 2 => 'OC\\Preview\\JPEG'," /var/www/html/config/config.php
+sed -i "/);/i 3 => 'OC\\Preview\\GIF'," /var/www/html/config/config.php
+sed -i "/);/i 4 => 'OC\\Preview\\BMP'," /var/www/html/config/config.php
+sed -i "/);/i 5 => 'OC\\Preview\\XBitmap'," /var/www/html/config/config.php
+sed -i "/);/i 6 => 'OC\\Preview\\MP3'," /var/www/html/config/config.php
+sed -i "/);/i 7 => 'OC\\Preview\\MP4'," /var/www/html/config/config.php
+sed -i "/);/i 8 => 'OC\\Preview\\TXT'," /var/www/html/config/config.php
+sed -i "/);/i 9 => 'OC\\Preview\\MarkDown'," /var/www/html/config/config.php
+sed -i "/);/i 10 => 'OC\\Preview\\PDF'," /var/www/html/config/config.php
+sed -i "/);/i )," /var/www/html/config/config.php
+
