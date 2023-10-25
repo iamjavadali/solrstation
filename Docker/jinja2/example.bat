f02c9be8965d76166f0a275c1db9c04e7bacc613
moneydontbuyclass <38925895+moneydontbuyclass@users.noreply.github.com>
2023-10-25 11:16:51 -0400

    updated Nginix proxy manager docker containers

--- Docker/jinja2/example.bat
+++
@@ -1,2 +0,0 @@
-:: This is an example invocation for yaml_jinja.py
-python yaml_jinja.py hello.yml hello.txt.j2 hello_out.txt
--- Docker/jinja2/example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This is an example invocation for yaml_jinja.py
-python3 yaml_jinja.py hello.yml hello.txt.j2 hello_out.txt
