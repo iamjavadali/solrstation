f38fc92c75dafa50dd156214abaca3d965c98893
moneydontbuyclass <38925895+moneydontbuyclass@users.noreply.github.com>
2023-11-14 11:54:41 -0500

    changed jenkins to apline linux

--- Docker/Jenkins/config.sh
+++ Docker/Jenkins/config.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 #Install JDK-17
-apt update
-apt install fontconfig openjdk-17-jre
-java -version
+#apt update
+#apt install fontconfig openjdk-17-jre
+#java -version
 
 
 #Install Jenkins
-wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt-get update
-apt-get install jenkins
+#wget -O /usr/share/keyrings/jenkins-keyring.asc \
+#  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+#echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#  https://pkg.jenkins.io/debian binary/ | sudo tee \
+#  /etc/apt/sources.list.d/jenkins.list > /dev/null
+#apt-get update
+#apt-get install jenkins
 
 #Starting and enabling Jenkins
-systemctl enable jenkins
-systemctl start jenkins
-systemctl status jenkins
+#systemctl enable jenkins
+#systemctl start jenkins
+#systemctl status jenkins
+
+
+apk update
+apk add jenkins -y
 
 #Display temp password for inital login and save to jenkins_pass.txt file
 cat /var/lib/jenkins/secrets/initialAdminPassword > /jenkins_pass.txt
