0f40a673ad88e328ab681c4162f13c0b4675012d
moneydontbuyclass <mr.javadali@gmail.com>
2024-01-05 02:00:24 -0500

    updated config.sh file

--- Docker/Nextcloud/NCImage/nextcloud/config.sh
+++ Docker/Nextcloud/NCImage/nextcloud/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt update && apt install ffmpeg -y
-apt-get update && apt-get install -y libbz2-dev
-docker-php-ext-install bz2 -y
+apt-get update && apt-get install libbz2-dev -y 
+docker-php-ext-install bz2
