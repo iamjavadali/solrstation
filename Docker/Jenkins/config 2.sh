6c17c3ebb458c8120eac9ae5fd878f955862a947
moneydontbuyclass <38925895+moneydontbuyclass@users.noreply.github.com>
2024-02-27 15:37:44 -0500

    updated portainer template

---
+++ Docker/Jenkins/config 2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Install JDK-17
+#apt update
+#apt install fontconfig openjdk-17-jre
+#java -version
+
+
+#Install Jenkins
+#wget -O /usr/share/keyrings/jenkins-keyring.asc \
+#  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+#echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#  https://pkg.jenkins.io/debian binary/ | sudo tee \
+#  /etc/apt/sources.list.d/jenkins.list > /dev/null
+#apt-get update
+#apt-get install jenkins
+
+#Starting and enabling Jenkins
+#systemctl enable jenkins
+#systemctl start jenkins
+#systemctl status jenkins
+
+
+apk update
+apk upgrade
+apk add openrc
+apk add jenkins
+service jenkins start
+#touch /run/openrc/softlevel
+
+
+#Display temp password for inital login and save to jenkins_pass.txt file
+#cat /var/lib/jenkins/secrets/initialAdminPassword > /jenkins_pass.txt
