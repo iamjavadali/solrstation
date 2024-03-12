b37eae23688c90c2aa3ee222bedd13d8a88e06cd
moneydontbuyclass <38925895+moneydontbuyclass@users.noreply.github.com>
2024-03-11 22:21:51 -0400

    added collabora/code container

--- Docker/collabora/collabora-code.sh
+++ Docker/collabora/collabora-code.sh
@@ -5,4 +5,4 @@ domain=office.solrstation.com
 server_name=cloud.solrstation.com
 
 sudo docker pull collabora/code
-sudo docker run --name collabora -t -d -p 9980:9980 -e “domain={$domain}” -e “server_name={$server_name}” --restart no --cap-add MKNOD collabora/code
+sudo docker run --name collabora -t -d -p 9980:9980 -e “domain=$domain” -e “server_name=$server_name” --restart no --cap-add MKNOD collabora/code
