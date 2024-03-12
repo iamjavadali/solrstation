97254459063e50da1023e8592e3edb93934f6f40
moneydontbuyclass <38925895+moneydontbuyclass@users.noreply.github.com>
2024-03-12 11:07:27 -0400

    added collabora/code container

--- Docker/collabora/collabora-code.sh
+++ Docker/collabora/collabora-code.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-domain=office.solrstation.com #Set this to the IP pointing to the collabora/code server
-server_name=192.168.86.58:9980 #Set this to the domain pointed to your collabora/code server
+domain=192.168.86.58:9980 #Set this to the IP pointing to the collabora/code server
+server_name=office.solrstation.com #Set this to the domain pointed to your collabora/code server
 user=admin
 pass=password
 tz=America/New_York
@@ -12,4 +12,4 @@ sudo docker pull collabora/code
 # sudo docker run --name collabora -t -d -p 9980:9980 -e “domain=$domain” -e “server_name=$server_name” --restart always --cap-add MKNOD collabora/code
 
 
-sudo docker run --name collabora -t -d -p 9980:9980 --privileged -e username=$user -e password=$pass -e “domain=$domain” -e TZ=$tz -e “server_name=$server_name” -e "aliasgroup1=https://cloud.solrstation.com|cloud.solrstation.com" --restart always --cap-add MKNOD collabora/code
+sudo docker run --name collabora -t -d -p 9980:9980 --privileged -e username=$user -e password=$pass -e “domain=$domain” -e TZ=$tz -e “server_name=$server_name” --restart always --cap-add MKNOD collabora/code
