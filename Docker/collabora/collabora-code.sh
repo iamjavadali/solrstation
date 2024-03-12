b8821ff12fe18bb07840f9e2927b622671d39929
moneydontbuyclass <38925895+moneydontbuyclass@users.noreply.github.com>
2024-03-12 09:33:35 -0400

    added collabora/code container

--- Docker/collabora/collabora-code.sh
+++ Docker/collabora/collabora-code.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 
-domain=office.solrstation.com #Set this to the domain pointing to the collabora/code server
-server_name=cloud.solrstation.com #Set this to the domain pointed to your nextcloud app
+domain=cloud.solrstation.com #Set this to the domain pointing to the collabora/code server
+server_name=office.solrstation.com #Set this to the domain pointed to your nextcloud app
 user=admin
 pass=password
+tz=TZ=America/New_York
+
 
 
 sudo docker pull collabora/code
 # sudo docker run --name collabora -t -d -p 9980:9980 -e “domain=$domain” -e “server_name=$server_name” --restart always --cap-add MKNOD collabora/code
 
 
-sudo docker run --name collabora -t -d -p 9980:9980 -e username=$user -e password=$pass -e “domain=$domain” -e “server_nam>
+sudo docker run --name collabora -t -d -p 9980:9980 --privileged -e username=$user -e password=$pass -e “domain=$domain” -e TZ=$tz -e “server_name=$server_name” --restart always --cap-add MKNOD collabora/code
